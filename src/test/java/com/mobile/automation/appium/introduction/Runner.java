ed4ed6fb52148e5f1315b04e9f12dfb88aee972d
itsvinayr <vinay.raghu10@gmail.com>
2019-03-13 23:10:49 +0530

    my first appium script

---
+++ src/test/java/com/mobile/automation/appium/introduction/Runner.java
@@ -0,0 +1,35 @@
+package com.mobile.automation.appium.introduction;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class Runner {
+	
+	public final static String APPIUM_URL = "http://127.0.0.1:4723/wd/hub";
+	public final static String BASE_URL = "http://google.com";
+	
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+		
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability(CapabilityType.BROWSER_NAME, "Chrome");
+		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android");
+		
+		WebDriver driver = new AndroidDriver(new URL(APPIUM_URL), capabilities);
+		driver.get(BASE_URL);
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		driver.findElement(By.name("q")).sendKeys("Hello Appium !!!");
+		Thread.sleep(3000L);
+		driver.quit();
+		
+	}
+
+}
