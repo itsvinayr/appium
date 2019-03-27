362e80f3aa2ca6fdafd45e908672d7144d102795
itsvinayr <vinay.raghu10@gmail.com>
2019-03-27 08:22:32 +0530

    adding apk installations and running installed apk's

--- src/test/java/com/mobile/automation/appium/selendroid/app/installation/Runner.java
+++ src/test/java/com/mobile/automation/appium/selendroid/app/installation/Runner.java
@@ -1,11 +1,10 @@
-package com.mobile.automation.appium.introduction;
+package com.mobile.automation.appium.selendroid.app.installation;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.AppiumDriver;
@@ -21,15 +20,13 @@ public class Runner {
 	
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		
+		File f = new File(System.getProperty("user.dir")+"//src//test//resources//apkFiles//selendroid-test-app-0.17.0.apk");
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability(CapabilityType.BROWSER_NAME, "Chrome");
 		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android");
+		capabilities.setCapability(MobileCapabilityType.APP, f.getAbsolutePath());
 		
 		driver = new AndroidDriver(new URL(APPIUM_URL), capabilities);
-		driver.get(BASE_URL);
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		driver.findElement(By.name("q")).sendKeys("Hello Appium !!!");
-		driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/button[2]")).click();
 		Thread.sleep(3000L);
 		driver.quit();
 		
--- src/test/java/com/mobile/automation/appium/selendroid/app/run/Runner.java
+++ src/test/java/com/mobile/automation/appium/selendroid/app/run/Runner.java
@@ -1,15 +1,15 @@
-package com.mobile.automation.appium.introduction;
+package com.mobile.automation.appium.selendroid.app.run;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class Runner {
@@ -21,15 +21,14 @@ public class Runner {
 	
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		
+		File f = new File(System.getProperty("user.dir")+"//src//test//resources//apkFiles//selendroid-test-app-0.17.0.apk");
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability(CapabilityType.BROWSER_NAME, "Chrome");
 		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android");
+		capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "io.selendroid.testapp");
+		capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".HomeScreenActivity");
 		
 		driver = new AndroidDriver(new URL(APPIUM_URL), capabilities);
-		driver.get(BASE_URL);
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		driver.findElement(By.name("q")).sendKeys("Hello Appium !!!");
-		driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/button[2]")).click();
 		Thread.sleep(3000L);
 		driver.quit();
 		
